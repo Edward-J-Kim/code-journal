98fb154dfa1365593e078650aeade97e8431550d
Edward Kim <ewakim90@gmail.com>
2023-06-05 15:57:11 +0000

    Completed issue-2-view-their-entries.

--- js/data.js
+++ js/data.js
@@ -6,3 +6,13 @@ var data = {
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', function (event) {
+  const dataJson = JSON.stringify(data);
+  localStorage.setItem('javascript-local-storage', dataJson);
+});
+
+const previousDataJson = localStorage.getItem('javascript-local-storage');
+if (previousDataJson !== null) {
+  data = JSON.parse(previousDataJson);
+}
