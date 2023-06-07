f6ddf68225b8e3dd8c5ad9e7953f5e014f66f791
Edward Kim <ewakim90@gmail.com>
2023-06-07 02:14:20 +0000

    Completed new-issue-2-view-their-entries exercise.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,118 @@
-const $imgLoad = document.querySelector('#user-photo-url');
-const $image = document.querySelector('#imgshow');
-$imgLoad.addEventListener('input', function (event) {
+const div = document.querySelector('.all-entries');
+const photoURL = document.getElementById('image-url');
+const entriesPage = document.getElementById('entries-page');
 
-  $image.setAttribute('src', event.target.value);
+photoURL.addEventListener('input', function (event) {
+  const img = document.querySelector('img');
+  img.src = event.target.value;
+});
+const submitForm = document.querySelector('form');
+submitForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const title = submitForm.elements.title.value;
+  const image = submitForm.elements.image.value;
+  const notes = submitForm.elements.notes.value;
+  const newObject = {
+    title,
+    image,
+    notes,
+    entryId: data.nextEntryId
+  };
+  data.entries.unshift(newObject);
+  data.nextEntryId++;
+  const img = document.querySelector('img');
+  img.src = './images/placeholder-image-square.jpg';
+  submitForm.reset();
+
+  const newEntry = renderEntry(newObject);
+  div.prepend(newEntry);
+  viewSwap('entries');
 });
 
-const $page = document.querySelector('#information');
+function renderEntry(entry) {
 
-function pageSubmit(event) {
-  event.preventDefault();
-  const entry = {
-    entryId: data.nextEntryId,
-    title: event.target.elements.title.value,
-    photoUrl: event.target.elements.url.value,
-    notes: event.target.elements.notes.value
-  };
+  const ul = document.createElement('ul');
+  ul.setAttribute('class', 'is-flex');
 
-  $page.reset();
-  data.nextEntryId++;
-  data.entries.unshift(entry);
-  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+  const imageLi = document.createElement('li');
+  imageLi.setAttribute('class', 'column-one-half');
+  ul.appendChild(imageLi);
+
+  const imageDiv = document.createElement('div');
+  imageLi.appendChild(imageDiv);
+
+  const image = document.createElement('img');
+  image.setAttribute('class', 'image-entry-desktop');
+  image.setAttribute('src', entry.image);
+  imageDiv.appendChild(image);
+
+  const textLi = document.createElement('li');
+  textLi.setAttribute('class', 'column-one-half padding');
+  ul.appendChild(textLi);
+
+  const textDiv = document.createElement('div');
+  textLi.appendChild(textDiv);
+
+  const title = document.createElement('h3');
+  title.textContent = entry.title;
+  textDiv.appendChild(title);
+
+  const notes = document.createElement('p');
+  notes.textContent = entry.notes;
+  textDiv.appendChild(notes);
+
+  return ul;
 }
 
-$page.addEventListener('submit', pageSubmit);
+document.addEventListener('DOMContentLoaded', function (event) {
+  if (data.entries.length === 0) {
+    toggleNoEntries();
+  }
+
+  viewSwap('entry-form');
+
+  document.getElementById('no-entries-paragraph').className = 'has-display-block';
+
+  for (let i = 0; i < data.entries.length; i++) {
+    const dataEntry = renderEntry(data.entries[i]);
+    div.appendChild(dataEntry);
+  }
+});
+
+function toggleNoEntries(event) {
+
+  if (data.entries.length === 0) {
+    entriesPage.className = 'has-display-none ';
+  } else {
+    entriesPage.className = 'entries-desktop';
+  }
+}
+
+function viewSwap(view) {
+  const entryForm = document.querySelector('.entry-form');
+
+  if (view === 'entries') {
+    entryForm.style.display = 'none';
+    entriesPage.className = 'entries-desktop';
+
+    if (data.entries.length > 0) {
+      document.getElementById('no-entries-paragraph').className = 'has-display-none';
+
+    }
+  }
+
+  if (view === 'entry-form') {
+    entryForm.style.display = 'block';
+    entriesPage.className = 'has-display-none';
+  }
+}
+
+const entriesLink = document.querySelector('.menu-entries');
+entriesLink.addEventListener('click', function (event) {
+  viewSwap('entries');
+});
+
+const newEntry = document.querySelector('.new-entries');
+newEntry.addEventListener('click', function (event) {
+  viewSwap('entry-form');
+});
